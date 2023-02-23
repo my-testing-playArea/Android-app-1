abe140d55d47e610a90efe1fb36ca77fbad1bb2c
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-23 15:17:23 +0530

    Add Button and click event

--- app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
+++ app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
@@ -2,12 +2,24 @@ package com.madhack.play.userexp.myfirstapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MyFirstActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_first_layout);
+
+        Button btn = findViewById(R.id.btnMyFirstButton);
+        btn.setText("My First Button");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MyFirstActivity","Button Clicked!");
+            }
+        });
     }
 
 }
