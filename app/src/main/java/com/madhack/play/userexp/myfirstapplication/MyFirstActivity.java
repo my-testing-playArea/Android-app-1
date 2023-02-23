61d49c6b2b795c5f61b3b2e0d7d4a13205d2ca84
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-23 16:34:50 +0530

    Create elements for translator app

--- app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
+++ app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
@@ -1,10 +1,12 @@
 package com.madhack.play.userexp.myfirstapplication;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MyFirstActivity extends Activity {
@@ -13,26 +15,18 @@ public class MyFirstActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_first_layout);
 
-        Button btn = findViewById(R.id.btnMyFirstButton);
-        btn.setText("My First Button");
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("MyFirstActivity", "Button Clicked!");
-            }
+        Button btnTranslate = findViewById(R.id.btn_translate);
+        TextView tvTranslatedText = findViewById(R.id.my_layout_text_view);
+        EditText etInputText = findViewById(R.id.my_layout_et_text);
+
+        btnTranslate.setText("Translate");
+        btnTranslate.setOnClickListener(v -> {
+            String text = etInputText.getText().toString();
+            tvTranslatedText.setText(text);
+
         });
 
-        final TextView textView = findViewById(R.id.my_layout_text_view);
-        textView.setText("Enter your text...");
 
-        findViewById(R.id.button).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.i("MyFirstActivity", "Hello World");
-                    }
-                }
-        );
     }
 
 }
