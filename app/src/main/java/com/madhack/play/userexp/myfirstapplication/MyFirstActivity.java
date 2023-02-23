996f24964ae793e733523396442d4a4bcb255cc2
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-23 14:33:01 +0530

    Add MyFirstActivity.java and my_first_layout.xml

--- app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
+++ app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
@@ -1,8 +1,13 @@
 package com.madhack.play.userexp.myfirstapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MyFirstActivity extends Activity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_first_layout);
+    }
 
 }
