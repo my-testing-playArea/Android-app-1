f4f21d856f83f9264ba5954cd79148cfa39e0ac5
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-23 15:44:19 +0530

    Add Textview

--- app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
+++ app/src/main/java/com/madhack/play/userexp/myfirstapplication/MyFirstActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MyFirstActivity extends Activity {
     @Override
@@ -17,9 +18,21 @@ public class MyFirstActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("MyFirstActivity","Button Clicked!");
+                Log.i("MyFirstActivity", "Button Clicked!");
             }
         });
+
+        final TextView textView = findViewById(R.id.my_layout_text_view);
+        textView.setText("Enter your text...");
+
+        findViewById(R.id.button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("MyFirstActivity", "Hello World");
+                    }
+                }
+        );
     }
 
 }
